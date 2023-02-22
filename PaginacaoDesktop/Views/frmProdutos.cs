f455eb7f6d8cb671e68eead684eb0a529405468a
Gustavo Duarte Santos <gustavoduartesantos@outlook.com.br>
2023-02-22 17:03:38 -0300

    Refatoração rotina de paginação.

--- PaginacaoDesktop/Views/frmProdutos.cs
+++ PaginacaoDesktop/Views/frmProdutos.cs
@@ -8,16 +8,14 @@ namespace PaginacaoDesktop.Views
 {
     public partial class frmProdutos : Form
     {
+        private int _deslocamento;
+        private int _tamanhoPagina = 5;
+        private int _maxTamanhoPagina = 10;
+        private int _totalRegistros;
+        private int _paginaAtual = 1;
+        private int _totalPaginas;
 
-        int Deslocamento;
 
-        int tamanhoPagina = 5;
-
-        int TotalRegistros;
-
-        int paginaAtual = 1;
-
-        int TotalPaginas;
 
         public frmProdutos() 
         {
@@ -31,19 +29,21 @@ namespace PaginacaoDesktop.Views
         {
             using (var ctx = new ProdutoDbContext())
             {
-               TotalRegistros = ctx.Produtos.Count();
+               _totalRegistros = ctx.Produtos.Count();
                var produtos = ctx.Produtos.OrderByDescending(p => p.ProdutoId)
-                    .Skip((paginaAtual - 1) * tamanhoPagina)
-                    .Take(tamanhoPagina)
+                    .Skip((_paginaAtual - 1) * _tamanhoPagina)
+                    .Take(_tamanhoPagina)
                     .ToList();
 
                 dgvProdutos.DataSource = produtos;
             }
 
-            TotalPaginas =  (TotalRegistros % tamanhoPagina) > 0 ? (TotalRegistros / tamanhoPagina) + 1 : TotalRegistros / tamanhoPagina;
+            _totalPaginas =  (_totalRegistros % _tamanhoPagina) > 0 
+                             ? (_totalRegistros / _tamanhoPagina) + 1  
+                             : _totalRegistros / _tamanhoPagina;
 
-            label2.Text = $"Página {paginaAtual} de {TotalPaginas}";
-            txtItensPorPagina.Text = tamanhoPagina.ToString();
+            label2.Text = $"Página {_paginaAtual} de {_totalPaginas}";
+            txtItensPorPagina.Text = _tamanhoPagina.ToString();
         }
 
         private void btnAnterior_Click(object sender, EventArgs e)
@@ -54,32 +54,12 @@ namespace PaginacaoDesktop.Views
                 return;
             }
 
-            var tamanhoPaginaAtual = Convert.ToInt32(txtItensPorPagina.Text);
+            int itens = Convert.ToInt32(txtItensPorPagina.Text);
+            ConfiguraPaginacao(itens, _maxTamanhoPagina, _totalRegistros, false);
 
-            TotalPaginas = (TotalRegistros % tamanhoPaginaAtual) > 0
-                            ? (TotalRegistros / tamanhoPaginaAtual) + 1
-                            : TotalRegistros / tamanhoPaginaAtual;
+            ListaProdutosPaginados(_deslocamento, _tamanhoPagina);
 
-
-            paginaAtual--;
-            Deslocamento = (paginaAtual - 1) * tamanhoPaginaAtual;
-            if (Deslocamento <= 0)
-            {
-                Deslocamento = 0;
-                paginaAtual = 1;
-            }
-     
-            using (var ctx = new ProdutoDbContext())
-            {
-                var produtos = ctx.Produtos.OrderByDescending(p => p.ProdutoId)
-                    .Skip(Deslocamento)
-                    .Take(tamanhoPaginaAtual)
-                    .ToList();
-
-                dgvProdutos.DataSource = produtos;
-            }
-
-            label2.Text = $"Página {paginaAtual} de {TotalPaginas}";
+            label2.Text = $"Página {_paginaAtual} de {_totalPaginas}";
         }
 
         private void btnProximo_Click(object sender, EventArgs e)
@@ -90,32 +70,14 @@ namespace PaginacaoDesktop.Views
                 return;
             }
 
-            var tamanhoPaginaAtual = Convert.ToInt32(txtItensPorPagina.Text);
+            int itens = Convert.ToInt32(txtItensPorPagina.Text);
+            ConfiguraPaginacao(itens, _maxTamanhoPagina, _totalRegistros, true);
 
-            TotalPaginas = (TotalRegistros % tamanhoPaginaAtual) > 0 
-                            ? (TotalRegistros / tamanhoPaginaAtual) + 1 
-                            : TotalRegistros / tamanhoPaginaAtual;
-            
-            paginaAtual++;
-            Deslocamento = (paginaAtual - 1) * tamanhoPaginaAtual;
-            if (Deslocamento >= TotalRegistros)
-            {
-                Deslocamento -= tamanhoPaginaAtual;
-                paginaAtual = TotalPaginas;
-            }
+            ListaProdutosPaginados(_deslocamento, _tamanhoPagina);
 
-            using (var ctx = new ProdutoDbContext())
-            {
-                var produtos = ctx.Produtos.OrderByDescending(p => p.ProdutoId)
-                    .Skip(Deslocamento)
-                    .Take(tamanhoPaginaAtual)
-                    .ToList();
-                dgvProdutos.DataSource = produtos;
-            }
-
-            label2.Text = $"Página {paginaAtual} de {TotalPaginas}";
-
+            label2.Text = $"Página {_paginaAtual} de {_totalPaginas}";
         }
+
 
         private void txtItensPorPagina_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -124,5 +86,54 @@ namespace PaginacaoDesktop.Views
                 e.Handled = true;
             }
         }
+
+
+        public void ListaProdutosPaginados(int deslocamento, int tamanhoPagina)
+        {
+            using (var ctx = new ProdutoDbContext())
+            {
+                var produtos = ctx.Produtos.OrderByDescending(p => p.ProdutoId)
+                    .Skip(deslocamento)
+                    .Take(tamanhoPagina)
+                    .ToList();
+                dgvProdutos.DataSource = produtos;
+            }
+        }
+
+
+        public void ConfiguraPaginacao(int itensPorPagina, int maxTamanhoPagina, int totalRegistros, bool proximo)
+        {
+            _tamanhoPagina = _tamanhoPagina > maxTamanhoPagina
+                     ? maxTamanhoPagina
+                     : itensPorPagina;
+
+            _totalPaginas = (totalRegistros % _tamanhoPagina) > 0
+                            ? (totalRegistros / _tamanhoPagina) + 1
+                            : totalRegistros / _tamanhoPagina;
+
+            if (proximo)
+            {
+                _paginaAtual++;
+                _deslocamento = (_paginaAtual - 1) * _tamanhoPagina;
+                if (_deslocamento >= _totalRegistros)
+                {
+                    _deslocamento -= _tamanhoPagina;
+                    _paginaAtual = _totalPaginas;
+                }
+            }
+            else
+            {
+                _paginaAtual--;
+                _deslocamento = (_paginaAtual - 1) * _tamanhoPagina;
+                if (_deslocamento <= 0)
+                {
+                    _deslocamento = 0;
+                    _paginaAtual = 1;
+                }
+            }
+        }
+
+
+
     }
 }
