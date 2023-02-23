e06f7a63c254dfc5f28c45c5174dc248cc128aa3
Gustavo Duarte Santos <gustavoduartesantos@outlook.com.br>
2023-02-23 15:24:25 -0300

    Criação da classe ParametrosPaginacao. Refatoracao de codigo.

---
+++ PaginacaoDesktop/Pagination/ParametrosPaginacao.cs
@@ -0,0 +1,74 @@
+﻿namespace PaginacaoDesktop.Pagination
+{
+    public class ParametrosPaginacao
+    {
+        const int maxItensPagina = 10;
+        public int NumeroPagina { get; private set; } = 1;
+        public int TotalPaginas { get; private set; }
+        public int Deslocamento { get; private set; }
+
+        private int _itensPagina = 5;
+        public int ItensPagina
+        {
+            get
+            {
+                return _itensPagina;
+            }
+            set
+            {
+                _itensPagina = (value > maxItensPagina) ? maxItensPagina : value;
+            }
+        }
+
+
+
+
+        public ParametrosPaginacao(int totalRegistros)
+        {
+            AtualizaTotalPaginas(totalRegistros);
+        }
+
+
+        private void AtualizaTotalPaginas(int totalRegistros)
+        {
+            TotalPaginas = (totalRegistros % _itensPagina) > 0 ? (totalRegistros / _itensPagina) + 1 : totalRegistros / _itensPagina;
+        }
+
+
+        private void AtualizaDeslocamento(bool proximo, int totalRegistros)
+        {
+            if (proximo)
+            {
+                NumeroPagina++;
+                Deslocamento = (NumeroPagina - 1) * _itensPagina;
+                if (Deslocamento >= totalRegistros)
+                {
+                    Deslocamento -= _itensPagina;
+                    NumeroPagina = TotalPaginas;
+                }
+            }
+            else
+            {
+                NumeroPagina--;
+                Deslocamento = (NumeroPagina - 1) * _itensPagina;
+                if (Deslocamento <= 0)
+                {
+                    Deslocamento = 0;
+                    NumeroPagina = 1;
+                }
+            }
+        }
+
+
+        public void ConfiguraPaginacao(int itensPorPagina, int totalRegistros, bool proximo) 
+        {
+            ItensPagina= itensPorPagina;
+            this.AtualizaTotalPaginas(totalRegistros);
+            this.AtualizaDeslocamento(proximo, totalRegistros);
+        }
+
+
+
+
+    }
+}
--- PaginacaoDesktop/Views/frmProdutos.cs
+++ PaginacaoDesktop/Views/frmProdutos.cs
@@ -1,4 +1,5 @@
 ﻿using PaginacaoDesktop.Context;
+using PaginacaoDesktop.Pagination;
 using System;
 using System.Data;
 using System.Linq;
@@ -8,12 +9,8 @@ namespace PaginacaoDesktop.Views
 {
     public partial class frmProdutos : Form
     {
-        private int _deslocamento;
-        private int _tamanhoPagina = 5;
-        private int _maxTamanhoPagina = 10;
         private int _totalRegistros;
-        private int _paginaAtual = 1;
-        private int _totalPaginas;
+        private ParametrosPaginacao _paginacao;
 
 
 
@@ -29,53 +26,26 @@ namespace PaginacaoDesktop.Views
         {
             using (var ctx = new ProdutoDbContext())
             {
-               _totalRegistros = ctx.Produtos.Count();
-               var produtos = ctx.Produtos.OrderByDescending(p => p.ProdutoId)
-                    .Skip((_paginaAtual - 1) * _tamanhoPagina)
-                    .Take(_tamanhoPagina)
-                    .ToList();
-
-                dgvProdutos.DataSource = produtos;
+                _totalRegistros = ctx.Produtos.Count();
             }
 
-            _totalPaginas =  (_totalRegistros % _tamanhoPagina) > 0 
-                             ? (_totalRegistros / _tamanhoPagina) + 1  
-                             : _totalRegistros / _tamanhoPagina;
+            _paginacao = new ParametrosPaginacao(_totalRegistros);
+            ListaProdutosPaginados(_paginacao.Deslocamento, _paginacao.ItensPagina);
 
-            label2.Text = $"Página {_paginaAtual} de {_totalPaginas}";
-            txtItensPorPagina.Text = _tamanhoPagina.ToString();
+            label2.Text = $"Página {_paginacao.NumeroPagina} de {_paginacao.TotalPaginas}";
+            txtItensPorPagina.Text = _paginacao.ItensPagina.ToString();
         }
+
 
         private void btnAnterior_Click(object sender, EventArgs e)
         {
-            if (txtItensPorPagina.Text == string.Empty || Convert.ToInt32(txtItensPorPagina.Text) == 0)
-            {
-                MessageBox.Show("O campo Itens por página deve conter um valor diferente de 0.");
-                return;
-            }
-
-            int itens = Convert.ToInt32(txtItensPorPagina.Text);
-            ConfiguraPaginacao(itens, _maxTamanhoPagina, _totalRegistros, false);
-
-            ListaProdutosPaginados(_deslocamento, _tamanhoPagina);
-
-            label2.Text = $"Página {_paginaAtual} de {_totalPaginas}";
+            RealizaPaginacao(false);
         }
+
 
         private void btnProximo_Click(object sender, EventArgs e)
         {
-            if (txtItensPorPagina.Text == string.Empty || Convert.ToInt32(txtItensPorPagina.Text) == 0 )
-            {
-                MessageBox.Show("O campo Itens por página deve conter um valor diferente de 0.");
-                return;
-            }
-
-            int itens = Convert.ToInt32(txtItensPorPagina.Text);
-            ConfiguraPaginacao(itens, _maxTamanhoPagina, _totalRegistros, true);
-
-            ListaProdutosPaginados(_deslocamento, _tamanhoPagina);
-
-            label2.Text = $"Página {_paginaAtual} de {_totalPaginas}";
+            RealizaPaginacao(true);
         }
 
 
@@ -88,6 +58,22 @@ namespace PaginacaoDesktop.Views
         }
 
 
+        public void RealizaPaginacao(bool proximo)
+        {
+            if (txtItensPorPagina.Text == string.Empty || Convert.ToInt32(txtItensPorPagina.Text) == 0)
+            {
+                MessageBox.Show("O campo Itens por página deve conter um valor diferente de 0.");
+                return;
+            }
+
+            _paginacao.ConfiguraPaginacao(Convert.ToInt32(txtItensPorPagina.Text), _totalRegistros, proximo);
+            ListaProdutosPaginados(_paginacao.Deslocamento, _paginacao.ItensPagina);
+
+            txtItensPorPagina.Text = _paginacao.ItensPagina.ToString(); 
+            label2.Text = $"Página {_paginacao.NumeroPagina} de {_paginacao.TotalPaginas}";
+        }
+
+
         public void ListaProdutosPaginados(int deslocamento, int tamanhoPagina)
         {
             using (var ctx = new ProdutoDbContext())
@@ -97,39 +83,6 @@ namespace PaginacaoDesktop.Views
                     .Take(tamanhoPagina)
                     .ToList();
                 dgvProdutos.DataSource = produtos;
-            }
-        }
-
-
-        public void ConfiguraPaginacao(int itensPorPagina, int maxTamanhoPagina, int totalRegistros, bool proximo)
-        {
-            _tamanhoPagina = _tamanhoPagina > maxTamanhoPagina
-                     ? maxTamanhoPagina
-                     : itensPorPagina;
-
-            _totalPaginas = (totalRegistros % _tamanhoPagina) > 0
-                            ? (totalRegistros / _tamanhoPagina) + 1
-                            : totalRegistros / _tamanhoPagina;
-
-            if (proximo)
-            {
-                _paginaAtual++;
-                _deslocamento = (_paginaAtual - 1) * _tamanhoPagina;
-                if (_deslocamento >= _totalRegistros)
-                {
-                    _deslocamento -= _tamanhoPagina;
-                    _paginaAtual = _totalPaginas;
-                }
-            }
-            else
-            {
-                _paginaAtual--;
-                _deslocamento = (_paginaAtual - 1) * _tamanhoPagina;
-                if (_deslocamento <= 0)
-                {
-                    _deslocamento = 0;
-                    _paginaAtual = 1;
-                }
             }
         }
 
